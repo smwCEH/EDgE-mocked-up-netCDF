f358d9bafd257a66cad94fd72af1773d6f16ff22
Simon Wright <smw@ceh.ac.uk>
2016-12-14 11:30:27 +0000

    Save image using scipy.misc.imsave (which relies on PIL apparently). Planning on changing to use GDAL to do the image stuff.

--- seasonal-forecast-netcdf.py
+++ seasonal-forecast-netcdf.py
@@ -23,6 +23,30 @@ import netCDF4
 from scipy.misc import imsave
 
 
+from osgeo import gdal
+import sys
+
+
+def describe_image(image):
+    # From: https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html
+    src_ds = gdal.Open(image)
+    if src_ds is None:
+        print 'Unable to open {0}'.format(image)
+        sys.exit(1)
+    print "[ RASTER BAND COUNT ]: ", src_ds.RasterCount
+    for band in range(src_ds.RasterCount):
+        band += 1
+        print "[ GETTING BAND ]: ", band
+        srcband = src_ds.GetRasterBand(band)
+        if srcband is None:
+            continue
+        stats = srcband.GetStatistics(True, True)
+        if stats is None:
+            continue
+        print "[ STATS ] =  Minimum=%.3f, Maximum=%.3f, Mean=%.3f, StdDev=%.3f" % ( \
+            stats[0], stats[1], stats[2], stats[3])
+
+
 def main():
     #
     #  Define netcdf files
@@ -47,89 +71,107 @@ def main():
     # Define netcdf variable and get array of data
     variable = nc.variables[netcdf_variable]
 
-    # time_slice_start, time_slice_end = 0, 228
-    # quintile_slice_start, quintile_slice_end = 0, 5
-    # lead_time_slice_start, lead_time_slice_end = 0, 6
-    # y_slice_start, y_slice_end = 0, 950
-    # x_slice_start, x_slice_end = 0, 1000
-    time_slice_start, time_slice_end = 0, 1
-    quintile_slice_start, quintile_slice_end = 0, 5
-    lead_time_slice_start, lead_time_slice_end = 0, 1
+    time_min, time_max = 1, 2
+    quintile_min, quintile_max = 1, 5
+    leadtime_min, leadtime_max = 1, 6
 
-    y_slice_start, y_slice_end = 0, 950
-    x_slice_start, x_slice_end = 0, 1000
-
+    # y_min, y_max = 0, 950
+    # y_size = y_max - y_min
+    # x_min, x_max = 0, 1000
+    # x_size = x_max - x_min
     resolution = 5000.
-
-    # y_box_max, y_box_min = 5127500., 5077500.
     y_box_max = 5167500.
     y_size = 100
     y_box_min = y_box_max - (y_size * resolution)
-    # x_box_min, x_box_max = 2782500., 2832500.
     x_box_min = 2762500.
     x_size = 100
     x_box_max = x_box_min + (x_size * resolution)
-
     y_variable = np.array(nc.variables['y'])
     print(y_variable.min(), y_variable.max())
     x_variable = np.array(nc.variables['x'])
     print(x_variable.min(), x_variable.max())
+    y_min = int((y_variable.max() - y_box_max) / resolution)
+    y_max   = int((y_variable.max() - y_box_min) / resolution)
+    print(y_min, y_max)
+    x_min = int((x_box_min - x_variable.min()) / resolution)
+    x_max   = int((x_box_max - x_variable.min()) / resolution)
+    print(x_min, x_max)
 
-    y_slice_start = int((y_variable.max() - y_box_max) / resolution)
-    y_slice_end   = int((y_variable.max() - y_box_min) / resolution)
-    print(y_slice_start, y_slice_end)
-    x_slice_start = int((x_box_min - x_variable.min()) / resolution)
-    x_slice_end   = int((x_box_max - x_variable.min()) / resolution)
-    print(x_slice_start, x_slice_end)
+    rgb = ma.empty((4, y_size, (x_size * leadtime_max)), dtype=np.uint8)
+    ma.set_fill_value(rgb, 255)
+    # print(rgb)
+    print('\n\n{0:<30}:\t{1}'.format('rgb.shape', rgb.shape))
+    print('{0:<30}:\t{1}'.format('rgb.dtype', rgb.dtype))
+    print('{0:<30}:\t{1}'.format('rgb.fill_value', rgb.fill_value))
 
-    # data = variable[time_slice_start:time_slice_end][quintile_slice_start:quintile_slice_end][lead_time_slice_start:lead_time_slice_end][y_slice_start:y_slice_end][x_slice_start:x_slice_end]
-    data = ma.array(variable[time_slice_start:time_slice_end,
-                            quintile_slice_start:quintile_slice_end,
-                            lead_time_slice_start:lead_time_slice_end,
-                            y_slice_start:y_slice_end,
-                            x_slice_start:x_slice_end])
-    print(type(data))
-    print(data.shape)
-    print(data.dtype)
-    print(data.fill_value)
-    # print(data)
+    print('\n\n')
+    for time in range(time_min, time_max + 1):
+        print('{0}{1:<12}:\t{2}'.format('\t' * 1, 'time', time))
+        for quintile in range(quintile_min, quintile_max + 1):
+            print('{0}{1:<12}:\t{2}'.format('\t' * 2, 'quintile', quintile))
+            for leadtime in range(leadtime_min, leadtime_max + 1):
+                print('{0}{1:<12}:\t{2}'.format('\t' * 3, 'leadtime', leadtime))
+
+                data = ma.array(variable[time - 1,
+                                        quintile - 1,
+                                        leadtime - 1,
+                                        y_min:y_max,
+                                        x_min:x_max])
+                # print('\n\n{0:<30}:\t{1}'.format('type(data)', type(data)))
+                # print('{0:<30}:\t{1}'.format('data.shape', data.shape))
+                # print('{0:<30}:\t{1}'.format('data.dtype', data.dtype))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'data.fill_value', data.fill_value))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'data.min()', data.min()))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'data.max()', data.max()))
+                # print(data)
+                #
+                scaled_data = np.rint(data).astype(int)
+                # print('\n\n{0:<30}:\t{1}'.format('scaled_data.fill_value', scaled_data.fill_value))
+                ma.set_fill_value(scaled_data, 255)
+                print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'scaled_data.fill_value', scaled_data.fill_value))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'scaled_data.min()', scaled_data.min()))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'scaled_data.max()', scaled_data.max()))
+                # print('{0:<30}:\t{1}'.format('scaled_data.dtype', scaled_data.dtype))
+                # print(scaled_data)
+
+                # summed_data = ma.sum(scaled_data, axis=1)
+                # print('\n\n{0:<30}:\t{1}'.format('summed_data.fill_value', summed_data.fill_value))
+                # ma.set_fill_value(summed_data, 255)
+                # print('{0:<30}:\t{1}'.format('summed_data.fill_value', summed_data.fill_value))
+                # print('{0:<30}:\t{1}'.format('summed_data.min()', summed_data.min()))
+                # print('{0:<30}:\t{1}'.format('summed_data.max()', summed_data.max()))
+                # print(summed_data)
+                # print(summed_data.mask)
+
+                rgb_x_min = (leadtime - 1) * x_size
+                rgb_x_max = (leadtime * x_size) - 1
+                # print(rgb_x_min, rgb_x_max)
+
+                # rgb[0, 0:y_size, rgb_x_min:rgb_x_max + 1] = scaled_data[0:y_size, 0:x_size]
+                # rgb[1, 0:y_size, rgb_x_min:rgb_x_max + 1] = scaled_data[0:y_size, 0:x_size]
+                # rgb[2, 0:y_size, rgb_x_min:rgb_x_max + 1] = scaled_data[0:y_size, 0:x_size]
+                # rgb[3, 0:y_size, rgb_x_min:rgb_x_max + 1] = scaled_data[0:y_size, 0:x_size]
+                if quintile < 5:
+                    rgb[quintile - 1, 0:y_size, rgb_x_min:rgb_x_max + 1] = scaled_data[0:y_size, 0:x_size]
+                # print(rgb)
+
+                del data, scaled_data
+
+        print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgb.fill_value', rgb.fill_value))
+        print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgb.min()', rgb.min()))
+        print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgb.max()', rgb.max()))
+
+        image_folder = r'E:\EDgE\seasonal-forecast\images'
+        image_file = 'junk{0}.png'.format(str(time).zfill(3))
+        image_path = os.path.join(image_folder, image_file)
+        imsave(image_path, rgb)
+
+        describe_image(image_path)
 
     nc.close()
 
-    scaled_data = np.rint(data).astype(int)
-    print(scaled_data.fill_value)
-    ma.set_fill_value(scaled_data, 255)
-    print(scaled_data.fill_value)
-    print(scaled_data)
-    print(scaled_data.dtype)
-
-    summed_data = np.sum(scaled_data, axis=1)
-    print(summed_data.fill_value)
-    ma.set_fill_value(summed_data, 255)
-    print(summed_data.fill_value)
-    print(summed_data)
-    print(summed_data.mask)
-
-    rgb = np.zeros((4, y_size, x_size), dtype=np.uint8)
-    # print(rgb)
-    print(rgb.shape)
-    print(rgb.dtype)
-    rgb[0, 0:y_size, 0:x_size] = scaled_data[0, 0, 0, 0:y_size, 0:x_size]
-    rgb[1, 0:y_size, 0:x_size] = scaled_data[0, 1, 0, 0:y_size, 0:x_size]
-    rgb[2, 0:y_size, 0:x_size] = scaled_data[0, 2, 0, 0:y_size, 0:x_size]
-    rgb[3, 0:y_size, 0:x_size] = scaled_data[0, 3, 0, 0:y_size, 0:x_size]
-    print(rgb)
-
-    image_folder = r'E:\EDgE\seasonal-forecast\images'
-    image_file = 'junk01.png'
-    image_path = os.path.join(image_folder, image_file)
-
-    imsave(image_path, rgb)
-
-
-
 if __name__ == '__main__':
 
-    np.set_printoptions(precision=4, threshold=501, suppress=True)
+    np.set_printoptions(precision=4, threshold=100001, suppress=True)
 
     main()
