6d24546c592123defdce769c32ae373204ff4a56
Simon Wright <smw@ceh.ac.uk>
2016-12-19 17:10:32 +0000

    Commit at the end of Monday. Sort out negative values/summed values > 100 on Tuesday.

--- seasonal-forecast-netcdf.py
+++ seasonal-forecast-netcdf.py
@@ -69,7 +69,7 @@ def main():
 
     STARTDATE = datetime.datetime.strptime('1900-01-01', '%Y-%m-%d')
 
-    output = False
+    output = True
 
     # Define netcdf dimensions bounds
     time_min, time_max = 1, 1
@@ -96,6 +96,143 @@ def main():
     # x_min = int((x_box_min - x_variable.min()) / resolution)
     # x_max   = int((x_box_max - x_variable.min()) / resolution)
     # print(x_min, x_max)
+
+    # Create netcdf4 dataset object
+    netcdf_folder = r'E:\EDgE\seasonal-forecast\data'
+    netcdf_file = r'ECMF_mHM_groundwater-recharge-probabilistic-quintile-distribution_monthly_1993_01_2012_05.nc'
+    netcdf_path = os.path.join(netcdf_folder, netcdf_file)
+    nc = netCDF4.Dataset(netcdf_path, 'r')
+
+    # Define netcdf variable and get array of data
+    variable = nc.variables['prob_quintile_dist']
+
+    # Define rgba array
+    rgba_bands = 4
+    rgba = np.zeros((rgba_bands, y_size, (x_size * leadtime_max)), dtype=np.uint8)
+    if output:
+        print('\n\n{0:<30}:\t{1}'.format('rgba.shape', rgba.shape))
+        print('{0:<30}:\t{1}'.format('rgba.dtype', rgba.dtype))
+
+    # Mock up time dimension as a list of floats
+    time_dim = [33968.0, 33999.0, 34027.0, 34058.0, 34088.0, 34119.0, 34149.0, 34180.0, 34211.0, 34241.0, 34272.0, 34302.0, 34333.0, 34364.0, 34392.0, 34423.0, 34453.0, 34484.0, 34514.0, 34545.0, 34576.0, 34606.0, 34637.0, 34667.0, 34698.0, 34729.0, 34757.0, 34788.0, 34818.0, 34849.0, 34879.0, 34910.0, 34941.0, 34971.0, 35002.0, 35032.0, 35063.0, 35094.0, 35123.0, 35154.0, 35184.0, 35215.0, 35245.0, 35276.0, 35307.0, 35337.0, 35368.0, 35398.0, 35429.0, 35460.0, 35488.0, 35519.0, 35549.0, 35580.0, 35610.0, 35641.0, 35672.0, 35702.0, 35733.0, 35763.0, 35794.0, 35825.0, 35853.0, 35884.0, 35914.0, 35945.0, 35975.0, 36006.0, 36037.0, 36067.0, 36098.0, 36128.0, 36159.0, 36190.0, 36218.0, 36249.0, 36279.0, 36310.0, 36340.0, 36371.0, 36402.0, 36432.0, 36463.0, 36493.0, 36524.0, 36555.0, 36584.0, 36615.0, 36645.0, 36676.0, 36706.0, 36737.0, 36768.0, 36798.0, 36829.0, 36859.0, 36890.0, 36921.0, 36949.0, 36980.0, 37010.0, 37041.0, 37071.0, 37102.0, 37133.0, 37163.0, 37194.0, 37224.0, 37255.0, 37286.0, 37314.0, 37345.0, 37375.0, 37406.0, 37436.0, 37467.0, 37498.0, 37528.0, 37559.0, 37589.0, 37620.0, 37651.0, 37679.0, 37710.0, 37740.0, 37771.0, 37801.0, 37832.0, 37863.0, 37893.0, 37924.0, 37954.0, 37985.0, 38016.0, 38045.0, 38076.0, 38106.0, 38137.0, 38167.0, 38198.0, 38229.0, 38259.0, 38290.0, 38320.0, 38351.0, 38382.0, 38410.0, 38441.0, 38471.0, 38502.0, 38532.0, 38563.0, 38594.0, 38624.0, 38655.0, 38685.0, 38716.0, 38747.0, 38775.0, 38806.0, 38836.0, 38867.0, 38897.0, 38928.0, 38959.0, 38989.0, 39020.0, 39050.0, 39081.0, 39112.0, 39140.0, 39171.0, 39201.0, 39232.0, 39262.0, 39293.0, 39324.0, 39354.0, 39385.0, 39415.0, 39446.0, 39477.0, 39506.0, 39537.0, 39567.0, 39598.0, 39628.0, 39659.0, 39690.0, 39720.0, 39751.0, 39781.0, 39812.0, 39843.0, 39871.0, 39902.0, 39932.0, 39963.0, 39993.0, 40024.0, 40055.0, 40085.0, 40116.0, 40146.0, 40177.0, 40208.0, 40236.0, 40267.0, 40297.0, 40328.0, 40358.0, 40389.0, 40420.0, 40450.0, 40481.0, 40511.0, 40542.0, 40573.0, 40601.0, 40632.0, 40662.0, 40693.0, 40723.0, 40754.0, 40785.0, 40815.0, 40846.0, 40876.0]
+    days = time_dim[0]
+
+    # Define image folder
+    image_folder = r'E:\EDgE\seasonal-forecast\images-20161219'
+
+    # Loop through dimensions to create images
+    print('\n\n')
+    for time in range(time_min, time_max + 1):
+        print('{0}{1}:\t{2}'.format('\t' * 1, 'time', time))
+        if output:
+            print('{0}{1}:\t{2}'.format('\t' * 1, 'days', days))
+        date = STARTDATE + datetime.timedelta(days=days)
+        print('{0}{1}:\t{2}'.format('\t' * 1, 'date', date.date().strftime('%Y%m%d')))
+
+        for quintile in range(quintile_min, quintile_max + 1):
+            print('{0}{1:<12}:\t{2}'.format('\t' * 2, 'quintile', quintile))
+            for leadtime in range(leadtime_min, leadtime_max + 1):
+                print('{0}{1:<12}:\t{2}'.format('\t' * 3, 'leadtime', leadtime))
+
+                # Read data slice from netcdf variable for time, quintile and leadtime into a floating point numpy masked array
+                float_data = ma.array(variable[time - 1,
+                                               quintile - 1,
+                                               leadtime - 1,
+                                               y_min:y_max,
+                                               x_min:x_max])
+                if output:
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'float_data.dtype',  float_data.dtype))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'float_data.min()',  float_data.min()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'float_data.max()',  float_data.max()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'float_data.mean()', float_data.mean()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'float_data.std()',  float_data.std()))
+
+                # Convert floating point numpy masked array to an integer (np.uint8) numpy masked array
+                integer_data = np.rint(float_data).astype(np.uint8)
+                if output:
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'integer_data.dtype',  integer_data.dtype))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'integer_data.min()',  integer_data.min()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'integer_data.max()',  integer_data.max()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'integer_data.mean()', integer_data.mean()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'integer_data.std()',  integer_data.std()))
+
+                # Convert integer numpy masked array to numpy array with masked values set to NODATA value
+                filled_data = integer_data.filled(fill_value=NODATA)
+                if output:
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'filled_data.dtype',  filled_data.dtype))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'filled_data.min()',  filled_data.min()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'filled_data.max()',  filled_data.max()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'filled_data.mean()', filled_data.mean()))
+                    print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'filled_data.std()',  filled_data.std()))
+
+                rgba_x_min = (leadtime - 1) * x_size
+                rgba_x_max = (leadtime * x_size)
+                # print(rgba_x_min, rgba_x_max)
+
+                # Copy
+                if quintile < 5:
+                    if output:
+                        print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'quintile', quintile - 1))
+                        print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'rgba_x_min', rgba_x_min))
+                        print('{0}{1:<30}:\t{2}'.format('\t' * 4, 'rgba_x_max', rgba_x_max))
+
+                    np.copyto(rgba[quintile - 1, 0:y_size, rgba_x_min:rgba_x_max], filled_data[0:y_size, 0:x_size], casting='same_kind')
+
+                    if output:
+                        print rgba[quintile - 1, 0:y_size, rgba_x_min:rgba_x_max].min(),\
+                              rgba[quintile - 1, 0:y_size, rgba_x_min:rgba_x_max].max(),\
+                              rgba[quintile - 1, 0:y_size, rgba_x_min:rgba_x_max].mean(),\
+                              rgba[quintile - 1, 0:y_size, rgba_x_min:rgba_x_max].std()
+
+                del float_data, integer_data, filled_data
+
+        if output:
+            print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'type(rgba)', type(rgba)))
+            print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgba.dtype', rgba.dtype))
+            print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgba.min()', rgba.min()))
+            print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgba.max()', rgba.max()))
+
+        for band in range(rgba.shape[0]):
+            if output:
+                print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgba[{0}].min()'.format(band),  rgba[band].min()))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgba[{0}].max()'.format(band),  rgba[band].max()))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgba[{0}].mean()'.format(band), rgba[band].mean()))
+                print('{0}{1:<30}:\t{2}'.format('\t' * 2, 'rgba[{0}].std()'.format(band),  rgba[band].std()))
+
+
+        summed_data = np.sum(rgba, axis=0)
+        # summed_data = np.sum(np.where(rgba <= 100), axis=0)
+        print('{0:<30}:\t{1}'.format('summed_data.shape', summed_data.shape))
+        print('{0:<30}:\t{1}'.format('summed_data.min()', summed_data.min()))
+        print('{0:<30}:\t{1}'.format('summed_data.max()', summed_data.max()))
+        # print(summed_data)
+
+
+            # if output:
+            #     histogram = np.histogram(rgba[band], bins=[-1,0,100,254,255])
+            #     print histogram
+            #     print histogram[0]
+            #     print histogram[0].sum()
+            #     unique, counts = np.unique(rgba[band], return_counts=True)
+            #     dictionary = dict(zip(unique, counts))
+            #     for key in sorted(dictionary):
+            #         print '{0}:\t{1}'.format(key, dictionary[key])
+
+        im = scipy.misc.toimage(rgba)
+        # im = scipy.misc.toimage(rgba, cmin=0, cmax=255)
+        # im = toimage(rgba, low=ma.min(rgba), high=ma.max(rgba))
+        image_file = 'cc-hm-ind-{0}.png'.format(date.date().strftime('%Y%m%d'))
+        image_path = os.path.join(image_folder, image_file)
+        print(image_path)
+        im.save(image_path)
+
+
+
+
+
+
+    sys.exit()
+
 
     tabs = 0
 
